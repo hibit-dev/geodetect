d38a61e7424c185d98e77bc2914c6ad1f1455f63
Tigran Mekinyan <tmekinyan@mail.ru>
2023-12-27 16:42:29 +0100

    Add Laravel service provider and discovery

---
+++ src/Laravel/GeoDetectServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hibit\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class GeoDetect
+ * @package Hibit\GeoDetect
+ */
+class GeoDetectServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    // php artisan vendor:publish --tag=hibit-geodetect
+    public function boot(): void
+    {
+        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'hibit');
+
+        $this->publishes([
+            __DIR__ . '/../../resources/lang/en/' => $this->app->langPath('en'),
+            __DIR__ . '/../../resources/lang/es/' => $this->app->langPath('es'),
+        ], 'hibit-geodetect');
+    }
+}
